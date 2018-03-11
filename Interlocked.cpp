d9e3ce578dd3b8e43e18c0d2a923081ef63ee70d
gu123go <1186529142@qq.com>
2018-03-11 14:44:35 +0800

    Interlocked

---
+++ Interlocked.cpp
@@ -0,0 +1,40 @@
+﻿/*
+** Author 	   : Gzh
+** Datetime    : 3/11
+** Description : 没有使用 Interlocked
+*/ 
+
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>        //_beginthreadex
+
+enum { THREAD_NUM = 10 };
+volatile long g_nLongCount;
+unsigned int __stdcall func(void* pM)
+{
+	Sleep(100);
+	g_nLongCount++;
+	Sleep(50);
+	
+	return 0;
+}
+
+int main()
+{
+	printf("\t原子操作\n");
+	int num = 50;     // 
+	int day = 0;
+	while(++day <= num){
+		g_nLongCount = 0;
+		int i;
+		HANDLE handle[THREAD_NUM];
+		for(int i = 0; i < THREAD_NUM; ++i){
+			handle[i] = (HANDLE)_beginthreadex(NULL, 0, func, NULL, 0, NULL);
+		} 
+		WaitForMultipleObjects(THREAD_NUM, handle, true, INFINITE);
+		printf("第%d 天有%d个用户登陆后记录结果是%d\n",day, THREAD_NUM, g_nLongCount);
+	}
+		
+	return 0;
+}
+ 
---
+++ Interlocked_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <process.h>
+#include <windows.h>
+
+enum { THREAD_NUM = 50 };
+volatile long g_nLongCount;
+unsigned __stdcall func(void *pM)
+{
+	Sleep(100);
+	InterlockedIncrement((long*)&g_nLongCount);          //原子操作
+	Sleep(50);
+	
+	return 0; 
+}
+
+int main()
+{
+	int num = 50;
+	int day = 0;
+	while(++day <= num){
+		g_nLongCount = 0;
+		HANDLE handle[THREAD_NUM];
+		
+		for(int i = 0; i < THREAD_NUM; ++i)
+			handle[i] = (HANDLE)_beginthreadex(NULL, 0, func, NULL, 0, NULL);
+		WaitForMultipleObjects(THREAD_NUM, handle, true, INFINITE);
+		printf("第%d天%d个用户登陆后记录结果是%d\n", day, THREAD_NUM, g_nLongCount);
+	}
+	
+	return 0;
+}
